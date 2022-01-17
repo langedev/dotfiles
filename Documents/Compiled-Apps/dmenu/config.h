eb0ef7f0cb63a062be30b79f6078ed86d23f7210
PanDoes <public@daltonlange.com>
2022-01-17 14:54:25 -0800

    Changed dmenu height and color to match polybar

--- Documents/Compiled-Apps/dmenu/config.h
+++ Documents/Compiled-Apps/dmenu/config.h
@@ -5,19 +5,22 @@ static int instant = 1;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"azuki_font:size=12"
+	"Cascadia Codes:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#D7DAE0", "#282E4A" },
-	[SchemeSel] = { "#282E4A", "#F0AFE1" },
-	[SchemeSelHighlight] = { "#282E4A", "#F0AFE1" },
-	[SchemeNormHighlight] = { "#ECBFBD", "#282E4A" },
-	[SchemeOut] = { "#D7DAE0", "#282E4A" },
+	[SchemeNorm] = { "#D9E0EE", "#1E1D2F" },
+	[SchemeSel] = { "#1E1D2F", "#F5C2E7" },
+	[SchemeSelHighlight] = { "#1E1D2F", "#F5C2E7" },
+	[SchemeNormHighlight] = { "#DDB6F2", "#1E1D2F" },
+	[SchemeOut] = { "#D9E0EE", "#1E1D2F" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words

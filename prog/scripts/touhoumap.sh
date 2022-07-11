37796c218745ecdea70f8679ef85fb1311509e4c
PanDoes <public@daltonlange.com>
2022-07-10 18:58:22 -0700

    Adds scripts, adds scripts to path

---
+++ prog/scripts/touhoumap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+xmodmap -e "keycode 25 = Up"
+xmodmap -e "keycode 39 = Down"
+xmodmap -e "keycode 38 = Left"
+xmodmap -e "keycode 40 = Right"
+
+xmodmap -e "keycode 44 = z Z z Z"
+xmodmap -e "keycode 45 = x X x X"
+xmodmap -e "keycode 46 = c C c C"
+xmodmap -e "keycode 47 = v V v V"
